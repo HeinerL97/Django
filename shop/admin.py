62108d865c30e381011e8f6eeb8e5c95b4c63a1a
Heiner Leonardo Urrego Gonzalez <127130082+HeinerL97@users.noreply.github.com>
2025-05-29 00:43:58 -0500

    primer commit

---
+++ shop/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Category
+from .models import Product
+
+# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'is_active')
+    list_filter = ('category', 'is_active')
+    search_fields = ('name', 'description')
+   
